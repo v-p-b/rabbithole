bdb1fc3b68df95d1d41f2cf354acad4fb17e7309
b <b@scrapco.de>
2026-02-26 10:23:24 +0100

    PyGhidra compatibility

--- rabbithole.py
+++ rabbithole.py
@@ -1,68 +1,95 @@
 # Mark functions with their cumulative cyclomatic complexity
 #@author buherator
-#@category _NEW_
+#@category CultOfDragon
 #@keybinding 
 #@menupath 
 #@toolbar 
+#@runtime PyGhidra
 
 from ghidra.program.util import CyclomaticComplexity
 from ghidra.util.task import TaskMonitor
 from ghidra.program.model.symbol import SourceType
 from docking.widgets import OptionDialog
 from ghidra.app.tablechooser import TableChooserExecutor, AddressableRowObject, ColumnDisplay
+from jpype import JImplements, JOverride
+from java.lang import String, Integer
 
 CYCLO_MAP = {}
 CCs = []
 cyclomaticComplexity = CyclomaticComplexity();
 
-class RabbitHoleExecutor(TableChooserExecutor):
+@JImplements(TableChooserExecutor)
+class RabbitHoleExecutor:
+    @JOverride
     def execute(self, rowObject):
         self.lastRowExecuted = rowObject
         return True
 
+    @JOverride
     def getButtonName(self):
         return "I'm late!"
 
-class RabbitHoleRow(AddressableRowObject):
+@JImplements(AddressableRowObject)
+class RabbitHoleRow():
     def __init__(self, func, cc):
         self.function = func
         #self.address = self.function.getEntryPoint()
         self.cc = cc
 
+    @JOverride
     def getAddress(self):
         return self.function.getEntryPoint()
 
+    @JOverride
     def getCycloComplexity(self):
         return self.cc
 
+    @JOverride
     def getName(self):
         return self.function.getName()
 
-class RabbitHoleCCColumn(ColumnDisplay):
+@JImplements(ColumnDisplay)
+class RabbitHoleCCColumn():
+
+    @JOverride
     def getColumnValue(self,rowObj):
         return rowObj.getCycloComplexity()
 
+    @JOverride
     def getColumnName(self):
         return "Cumulative Complexity"  
 
+    @JOverride
     def getColumnClass(self):
-        return int
+        return Integer
 
+    @JOverride
     def compare(self, r1, r2):
         c1 = r1.getCycloComplexity()
         c2 = r2.getCycloComplexity()
         return c1-c2
 
-class RabbitHoleNameColumn(ColumnDisplay):
+    @JOverride
+    def equals(self, other):
+        return self.compare(self, other) == 0
+
+
+@JImplements(ColumnDisplay)
+class RabbitHoleNameColumn():
+
+    @JOverride
     def getColumnValue(self,rowObj):
         return rowObj.getName()
 
+    @JOverride
     def getColumnName(self):
         return "Function name"  
 
+    @JOverride
     def getColumnClass(self):
-        return str
+        return String
 
+    @JOverride
     def compare(self, r1, r2):
         n1 = r1.getName()
         n2 = r2.getName()
@@ -72,6 +99,10 @@ class RabbitHoleNameColumn(ColumnDisplay):
             return 1
         else:
             return 0
+
+    @JOverride
+    def equals(self, other):
+        return self.compare(self, other) == 0
 
 def recurse_cyclo(func, visited):
     entry=func.getEntryPoint().getOffset()
